3fbbc9a6e1865b74dc4dc68e804ae38faeed90b5
Yashvesh20 <yashvesh20@gmail.com>
2025-08-03 16:49:55 +0530

    Create Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,22 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+int main(){
+    int num[10];
+    cout <<"Enter any 5 integer values: ";
+    for(int i=0;i<5;i++){
+        cin>>num[i];
+    }
+    for(int j=0;j<5;j++)
+    {
+        cout<<num[j]<<" ";
+    }
+}
+Output:
+Enter any 5 integer values: 20
+22
+220
+78
+90
+
+20 22 220 78 90 
