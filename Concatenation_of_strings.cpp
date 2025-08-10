c05e00761eefb443fc85c9ee9fe38112d128890d
Yashvesh20 <yashvesh20@gmail.com>
2025-08-10 21:57:20 +0530

    Create Concatenation_of_strings.cpp

---
+++ Concatenation_of_strings.cpp
@@ -0,0 +1,17 @@
+Yashvesh Singh 24070123138
+#include <iostream>
+using namespace std;
+
+int main(){
+   string str,str2,str3;
+   cout<<"Enter a String: ";
+   cin>>str;
+   cout<<"Enter a String for concatenation : ";
+   cin>>str2;
+   str3=str+str2;
+   cout<<str3;
+}
+output:
+Enter a String: Yashvesh
+Enter a String for concatenation : Singh
+YashveshSingh
