3435f789860d17c9dfba934d1477d2cb9ecc36d0
Yashvesh20 <yashvesh20@gmail.com>
2025-08-10 21:59:56 +0530

    Create Display_String.cpp

---
+++ Display_String.cpp
@@ -0,0 +1,13 @@
+Yashvesh Singh 24070123138
+#include <iostream>
+using namespace std;
+
+int main(){
+   string str;
+   cout<<"Enter a String: ";
+   cin>>str;
+   cout<<str<<endl;
+}
+Output:
+Enter a String: Yashvesh20
+Yashvesh20
