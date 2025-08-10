d8e09243bbb9425bc026bc6d27f64ea7d5ae02f4
Yashvesh20 <yashvesh20@gmail.com>
2025-08-10 22:07:42 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+Yashvesh Singh 24070123138
+#include <iostream>
+using namespace std;
+
+int main(){
+   string str;
+   int n;
+   cout<<"Enter a String: ";
+   cin>>str;
+   n=str.length();
+   for(int i=n-1;i>=0;i--)
+   {
+       cout<<str[i];
+   }
+}
+Output:
+Enter a String: Yashvesh
+hsevhsaY
